e4c786455ca16f53c26b93a94d01a8fffe123556
15806905685 <2271918385@qq.com>
2016-11-26 16:22:08 +0800

    i修改项目icon

--- app/src/main/java/com/example/jetwang/myfirstgitre/MainActivity.java
+++ app/src/main/java/com/example/jetwang/myfirstgitre/MainActivity.java
@@ -2,24 +2,66 @@ package com.example.jetwang.myfirstgitre;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<String> data;
+    private RecycleAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initView();
-        initData();   
+        initData();
+        recyclerView = ((RecyclerView) findViewById(R.id.recycle_view));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter = new RecycleAdapter());
+
     }
 
-    private void initData() {
-
+    public void initData(){
+        data = new ArrayList<String>();
+        for(int i = 1; i < 30; i++){
+            data.add(String.valueOf(i));
+        }
     }
 
-    private void initView() {
-        TextView xx = new TextView(this);
+    class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHolder>{
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.recycle_view_item, parent, false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.txt.setText(data.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView txt;
+
+            public MyViewHolder(View view){
+                super(view);
+                txt = ((TextView) view.findViewById(R.id.item_id));
+            }
+        }
     }
+
 }
